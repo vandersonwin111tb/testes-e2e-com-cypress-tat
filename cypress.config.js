9fdd81610cb2a2b068b9304d9f7f34b5b8fad6e6
vandersonwin111tb <vandersonwin111tb@gmail.com>
2025-12-05 18:42:28 -0300

    Create e2e tests with cypress

--- cypress.config.js
+++ cypress.config.js
@@ -7,8 +7,8 @@ module.exports = defineConfig({
     env: {
       viewportWidthBreakpoint: 768,
     },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 })
