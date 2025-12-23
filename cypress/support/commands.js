366aa5d03ecdc2d235090e2afd92b759c003f089
vandersonwin111tb <vandersonwin111tb@gmail.com>
2025-12-23 11:10:47 -0300

    Criando testes e2e com cypress

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,7 +24,7 @@ Cypress.Commands.add('guiLogin', (
   cy.get('#email').type(username)
   cy.get('#password').type(password, { log: false })
   cy.contains('button', 'Login').click()
-  cy.wait('@getNotes', { timeout: 10000 })
+  cy.wait('@getNotes', { timeout: 20000 })
   cy.contains('h1', 'Your Notes').should('be.visible')
 })
 
